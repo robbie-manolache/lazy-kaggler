149fac9d354ba2bcc15843b2779a99790fd4ee02
robbie29 <37903975+robbie29@users.noreply.github.com>
2020-08-09 17:30:00 +1000

    conduct check on whether new files were added from kernel output download

--- lazykaggler/kernels.py
+++ lazykaggler/kernels.py
@@ -1,12 +1,14 @@
 import os
 import subprocess
+from numpy import setdiff1d
 
 def kernel_output_download(user, kernel_name, local_path = None):
     """
     """
 
-    # set kernel path
+    # set kernel path and record initial files
     kernel_path = "/".join([user, kernel_name])
+    files_before = os.listdir(local_path)
 
     # set up command
     cmd = " ".join(["kaggle kernels output", kernel_path])
@@ -16,5 +18,13 @@ def kernel_output_download(user, kernel_name, local_path = None):
     # run command
     subprocess.run(cmd)
 
-    # check local path contents
-    print(os.listdir(local_path))
+    # check change local path contents
+    files_after = os.listdir(local_path)
+    new_files = list(setdiff1d(files_after, files_before))
+    if len(new_files) > 0:
+        for f in new_files:
+            print("%s has been added to local path"%f)
+    else:
+        print("No new files added. " +
+              "Either existing files were updated " +
+              "or kernel does not have any output/exist.")
